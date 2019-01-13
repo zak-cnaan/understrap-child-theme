44aa5fceb78b6499ed4e23d4ffdc43a5c2f93ceb
zak cnaan <zak@zakcnaan.com>
2019-01-13 09:17:15 +0200

    saving

--- functions.php
+++ functions.php
@@ -26,7 +26,7 @@ function theme_enqueue_styles() {
 		wp_register_script('jquery', 'https://code.jquery.com/jquery-3.3.1.min.js', false, '3.3.1');
         wp_enqueue_script('jquery');
         
-	wp_enqueue_script( 'popper-scripts', get_template_directory_uri() . '/js/popper.min.js', array(), false);
+	/*wp_enqueue_script( 'popper-scripts', get_template_directory_uri() . '/js/popper.min.js', array(), false);*/
     wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $the_theme->get( 'Version' ), true );
     if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
         wp_enqueue_script( 'comment-reply' );
--- gulpfile.js
+++ gulpfile.js
@@ -141,7 +141,7 @@ gulp.task( 'scripts', function() {
 
         // End - All BS4 stuff
 
-        paths.dev + '/js/skip-link-focus-fix.js',
+        //paths.dev + '/js/skip-link-focus-fix.js',
 
         // Adding currently empty javascript file to add on for your own themes´ customizations
         // Please add any customizations to this .js file only!
